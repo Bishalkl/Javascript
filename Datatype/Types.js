cdbe73fd42d8a3799c43d14f61cc2913336b1f72
Bishalkl <bishal123koirala@gmail.com>
2023-10-11 23:31:22 +0545

    done
    done

---
+++ Datatype/Types.js
@@ -0,0 +1,61 @@
+// Primitives Datatype
+// string
+// number
+// boolean
+// null
+// undefined
+
+// Reference Datatype
+// Arrays
+// Object literal
+// function 
+// date
+
+
+// primitives
+
+let string = "Bishal Koirala"; // string
+console.log(typeof string);
+
+let number = 23.4; // numbers
+console.log(typeof number)
+
+let I_am_male = true;
+console.log(typeof I_am_male); // booleanr
+
+let test = null; // null
+console.log(typeof test)
+
+let a; // undefined
+console.log(typeof a)
+
+
+// Reference Datatype
+
+// Arrays
+myArrays = [1 , "Bishal", true]; 
+console.log(typeof myArrays) // object
+
+// Object Literal
+let information = { 
+    age : 12,
+    name : "Bishal koirala",
+    marks : 98.4
+}
+console.log(typeof information) // object
+
+// funnction
+function calcArea(a, b){
+    
+}
+console.log(typeof calcArea) //function
+
+// date
+let newDate = new Date();
+console.log(newDate)
+console.log(typeof newDate) //object
+
+
+
+
+
